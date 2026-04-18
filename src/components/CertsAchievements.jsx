32148172b1f49d78d06efafe234075ef29930f17
Vedant Tapkir <47036160+Octaflick@users.noreply.github.com>
2026-04-18 10:26:46 +0530

    Changes: Minor UI fixes

--- src/components/CertsAchievements.jsx
+++ src/components/CertsAchievements.jsx
@@ -4,15 +4,21 @@ export default function CertsAchievements() {
 			<fieldset>
 				<h3>Certification & Acheivements</h3>
 				<ul>
+                    <li>
+						<h4>MumbaiHacks 2025</h4>
+						<a href='https://drive.google.com/file/d/1lCGFz6EFXXOzDK1cpBTMNW-aD0HXBGfi/view'>View Participation Certificate</a>
+					</li>
+                    <li>
+						<h4>Supercontributor Hacktoberfest 2025</h4>
+						<a href='https://www.holopin.io/@octaflick#'>View</a>
+					</li>
 					<li>
 						<h4>Certified Python Developer</h4>
 						<a href='media/python_certificate.pdf'>View</a>
 					</li>
-					<li>
-						<h4>Supercontributor Hacktoberfest 2025</h4>
-						<a href='https://www.holopin.io/@octaflick#'>View</a>
-					</li>
 				</ul>
+
+                
 			</fieldset>
 		</div>
 	);
