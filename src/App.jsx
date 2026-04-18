a083517d14db37f7dc76ed1626aaf65e9ac4a380
Vedant Tapkir <47036160+Octaflick@users.noreply.github.com>
2026-04-18 09:12:08 +0530

    Reactified: About me

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import { useState, useEffect } from "react";
 import DisplayPicture from "./components/DisplayPicture";
+import AboutMe from "./components/AboutMe";
 
 export default function App() {
 	return (
 		<>
 			<DisplayPicture />
+      <AboutMe />
 		</>
 	);
 }
---
+++ src/components/AboutMe.jsx
@@ -0,0 +1,24 @@
+export default function AboutMe() {
+	return (
+		<div className='about-me'>
+			<fieldset>
+				<h3>About me</h3>
+				<p>
+					A Tech-Enhusiast, Glass Half-full Guy ! Started developing my passion
+					into real work since i was 10.
+				</p>
+				<br />
+				<ul>
+					<li>
+						Wrote a <a href='http://superbsav.blogspot.com/'>Blog</a> and
+						learned HTML along the way.
+					</li>
+					<li>
+						Created a Python Based GUI project 'School Library Management
+						System' gaining knowledge on how to use pandas
+					</li>
+				</ul>
+			</fieldset>
+		</div>
+	);
+}
