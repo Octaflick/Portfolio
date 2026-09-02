3da900cb13ca05331a151db90784d7cdb054b365
Vedant Tapkir <47036160+Octaflick@users.noreply.github.com>
2026-09-02 21:28:52 +0530

    Add: Leetcode,Codeforces links to profile

--- src/components/DisplayHeader.jsx
+++ src/components/DisplayHeader.jsx
@@ -1,12 +1,16 @@
 import linkedIn_Logo from "../assets/linkedin.svg"
 import github_Logo from "../assets/github.svg"
 import mail_Logo from "../assets/envelope.svg"
+import codeforces_Logo from "../assets/codeforces.svg"
+import leetcode_Logo from "../assets/leetcode.svg"
 import pfp from "../assets/pfp.jpg";
 
 export default function DisplayPicture({img_url=pfp}){
     let linkedIn = "https://www.linkedin.com/in/vedant-tapkir-62a42320b/"
     let github = "https://github.com/Octaflick"
-    let mailId = "mailto:vedanttapkir@gmail.com"
+  let mailId = "mailto:vedanttapkir@gmail.com"
+  let codeforcesId = "https://codeforces.com/profile/vedanttapkir"
+  let leetcodeId = "https://leetcode.com/u/VedantTapkir/"
     return (
         <div className="profile-photo">
         <img id="pfp" src={img_url} alt="Profile Photo"/>
@@ -14,7 +18,10 @@ export default function DisplayPicture({img_url=pfp}){
         <div className="contact">
             <a href={linkedIn}><img src={linkedIn_Logo} alt="linkedin"/></a>
             <a href={github}><img src={github_Logo}/></a>
-            <a href={mailId}><img src={mail_Logo}/></a>
+          <a href={codeforcesId}><img src={codeforces_Logo} /></a>
+          <a href={leetcodeId}><img src={leetcode_Logo} /></a>
+          <a href={mailId}><img src={mail_Logo} /></a>
+          
         </div>
         </div>
     )
